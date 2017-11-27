b364cf52503fde07786ebfb7f4a25e7413a35c3b
Hussam Elemmawi <hussamelemmawi@Hussams-MacBook-Pro.local>
2017-11-27 17:19:20 +0200

    fix app not building

--- app/src/main/java/com/hussamelemmawi/nanodegree/marvelpeople/data/source/remote/ApiUtility.java
+++ app/src/main/java/com/hussamelemmawi/nanodegree/marvelpeople/data/source/remote/ApiUtility.java
@@ -1,5 +1,6 @@
 package com.hussamelemmawi.nanodegree.marvelpeople.data.source.remote;
 
+
 import com.hussamelemmawi.nanodegree.marvelpeople.BuildConfig;
 
 import java.security.MessageDigest;
@@ -12,65 +13,65 @@ import java.util.HashMap;
 
 public class ApiUtility {
 
-  static final String MARVEL_BASE_URL = "https://gateway.marvel.com:443/v1/public/";
+    static final String MARVEL_BASE_URL = "https://gateway.marvel.com:443/v1/public/";
 
-  static final String CHARACTERS_ENDPOINT = "characters";
+    static final String CHARACTERS_ENDPOINT = "characters";
 
-  static final String COMICS_ENDPOINT = "comics";
+    static final String COMICS_ENDPOINT = "comics";
 
-  static final String LIMIT_PARAM = "limit";
-  static final String OFFSET_PARAM = "offset";
-  static final String TIMESTAMP_PARAM = "ts";
-  static final String PUBLIC_API_KEY_PARAM = "apikey";
-  static final String HASH_PARAM = "hash"; // hash = md5(ts + privateKey + publicKey) in order
+    static final String LIMIT_PARAM = "limit";
+    static final String OFFSET_PARAM = "offset";
+    static final String TIMESTAMP_PARAM = "ts";
+    static final String PUBLIC_API_KEY_PARAM = "apikey";
+    static final String HASH_PARAM = "hash"; // hash = md5(ts + privateKey + publicKey) in order
 
-  static HashMap<String, String> getRequestQueryParam(int lastHeroId) {
-    String limit = "20";
-    String offset = String.valueOf(lastHeroId);
-    String timeStamp = Long.toString(System.currentTimeMillis() / 1000);
-    String hash = md5(getHash(timeStamp));
+    static HashMap<String, String> getRequestQueryParam(int lastHeroId) {
+        String limit = "20";
+        String offset = String.valueOf(lastHeroId);
+        String timeStamp = Long.toString(System.currentTimeMillis() / 1000);
+        String hash = md5(getHash(timeStamp));
 
-    HashMap<String, String> options = new HashMap<>();
+        HashMap<String, String> options = new HashMap<>();
 
-    options.put(ApiUtility.LIMIT_PARAM, limit);
-    options.put(ApiUtility.OFFSET_PARAM, offset);
-    options.put(ApiUtility.TIMESTAMP_PARAM, timeStamp);
-    options.put(ApiUtility.PUBLIC_API_KEY_PARAM, BuildConfig.MARVEL_PUBLIC_API_KEY);
-    options.put(ApiUtility.HASH_PARAM, hash);
+        options.put(ApiUtility.LIMIT_PARAM, limit);
+        options.put(ApiUtility.OFFSET_PARAM, offset);
+        options.put(ApiUtility.TIMESTAMP_PARAM, timeStamp);
+        options.put(ApiUtility.PUBLIC_API_KEY_PARAM, BuildConfig.MARVEL_PUBLIC_API_KEY);
+        options.put(ApiUtility.HASH_PARAM, hash);
 
-    return options;
-  }
-
-  private static String getHash(String timeStamp) {
-    return timeStamp
-      .concat(BuildConfig.MARVEL_PRIVATE_API_KEY)
-      .concat(BuildConfig.MARVEL_PUBLIC_API_KEY);
-  }
-
-  private static String md5(final String s) {
-    final String MD5 = "MD5";
-    try {
-      // Create MD5 Hash
-      MessageDigest digest = java.security.MessageDigest
-        .getInstance(MD5);
-      digest.update(s.getBytes());
-      byte messageDigest[] = digest.digest();
-
-      // Create Hex String
-      StringBuilder hexString = new StringBuilder();
-      for (byte aMessageDigest : messageDigest) {
-        String h = Integer.toHexString(0xFF & aMessageDigest);
-        while (h.length() < 2)
-          h = "0" + h;
-        hexString.append(h);
-      }
-      return hexString.toString();
-
-    } catch (NoSuchAlgorithmException e) {
-      e.printStackTrace();
+        return options;
     }
-    return "";
-  }
+
+    private static String getHash(String timeStamp) {
+        return timeStamp
+                .concat(BuildConfig.MARVEL_PRIVATE_API_KEY)
+                .concat(BuildConfig.MARVEL_PUBLIC_API_KEY);
+    }
+
+    private static String md5(final String s) {
+        final String MD5 = "MD5";
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest
+                    .getInstance(MD5);
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuilder hexString = new StringBuilder();
+            for (byte aMessageDigest : messageDigest) {
+                String h = Integer.toHexString(0xFF & aMessageDigest);
+                while (h.length() < 2)
+                    h = "0" + h;
+                hexString.append(h);
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
 
 
 }
--- app/src/main/java/com/hussamelemmawi/nanodegree/marvelpeople/data/source/remote/RemoteDataSource.java
+++ app/src/main/java/com/hussamelemmawi/nanodegree/marvelpeople/data/source/remote/RemoteDataSource.java
@@ -27,92 +27,92 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 
 public class RemoteDataSource implements DataSource.Remote {
-  private MarvelApi mMarvelApi;
+    private MarvelApi mMarvelApi;
 
-  public RemoteDataSource() {
-    Retrofit retrofit = new Retrofit.Builder()
-      .baseUrl(ApiUtility.MARVEL_BASE_URL)
-      .addConverterFactory(GsonConverterFactory.create())
-      .build();
+    public RemoteDataSource() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(ApiUtility.MARVEL_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-    mMarvelApi = retrofit.create(MarvelApi.class);
-  }
-
-  @Override
-  public void getHeroes(@NonNull final FetchDataCallback callback, int fromHeroId) {
-    Call<MarvelResponseModel<HeroResponseResult>> call = mMarvelApi.listOfHeroesResponse(ApiUtility.getRequestQueryParam(fromHeroId));
-    call.enqueue(new Callback<MarvelResponseModel<HeroResponseResult>>() {
-      @Override
-      public void onResponse(Call<MarvelResponseModel<HeroResponseResult>> call, Response<MarvelResponseModel<HeroResponseResult>> response) {
-        callback.onSuccess(mapHeroResponseResultsIntoHeroes(response.body().getData().getResults()));
-      }
-
-      @Override
-      public void onFailure(Call<MarvelResponseModel<HeroResponseResult>> call, Throwable t) {
-        callback.onFailure();
-      }
-    });
-  }
-
-  @Override
-  public void getComics(@NonNull final FetchDataCallback callback, int fromComicId) {
-    Call<MarvelResponseModel<ComicResponseResult>> call = mMarvelApi.listOfComicsResponse(ApiUtility.getRequestQueryParam(fromComicId));
-    call.enqueue(new Callback<MarvelResponseModel<ComicResponseResult>>() {
-      @Override
-      public void onResponse(Call<MarvelResponseModel<ComicResponseResult>> call, Response<MarvelResponseModel<ComicResponseResult>> response) {
-        callback.onSuccess(mapComicResponseResultsIntoComics(response.body().getData().getResults()));
-      }
-
-      @Override
-      public void onFailure(Call<MarvelResponseModel<ComicResponseResult>> call, Throwable t) {
-        callback.onFailure();
-      }
-    });
-  }
-
-  private List<Hero> mapHeroResponseResultsIntoHeroes(List<HeroResponseResult> heroResponseResultList) {
-    List<Hero> heroList = new ArrayList<>();
-    Hero.Builder builder;
-    for (HeroResponseResult hrr : heroResponseResultList) {
-      builder = new Hero.Builder(hrr.getId(), hrr.getName())
-        .description(hrr.getDescription())
-        .thumbnailPath(hrr.getThumbnail().getPath())
-        .thumbnailExtention(hrr.getThumbnail().getExtension());
-
-      for (MarvelUrl marvelUrl : hrr.getUrls()) {
-        if (marvelUrl.getType().equals("detail")) {
-          builder.detailsUrl(marvelUrl.getUrl());
-        }
-      }
-      heroList.add(builder.build());
+        mMarvelApi = retrofit.create(MarvelApi.class);
     }
-    return heroList;
-  }
 
-  private List<Comic> mapComicResponseResultsIntoComics(List<ComicResponseResult> comicResponseResultList) {
-    List<Comic> comicList = new ArrayList<>();
+    @Override
+    public void getHeroes(@NonNull final FetchDataCallback callback, int fromHeroId) {
+        Call<MarvelResponseModel<HeroResponseResult>> call = mMarvelApi.listOfHeroesResponse(ApiUtility.getRequestQueryParam(fromHeroId));
+        call.enqueue(new Callback<MarvelResponseModel<HeroResponseResult>>() {
+            @Override
+            public void onResponse(Call<MarvelResponseModel<HeroResponseResult>> call, Response<MarvelResponseModel<HeroResponseResult>> response) {
+                callback.onSuccess(mapHeroResponseResultsIntoHeroes(response.body().getData().getResults()));
+            }
 
-    Comic.Builder builder;
-    for (ComicResponseResult crr : comicResponseResultList) {
-      builder = new Comic.Builder(crr.getId(), crr.getTitle())
-        .description(crr.getDescription())
-        .thumbnailPath(crr.getThumbnail().getPath())
-        .thumbnailExtention(crr.getThumbnail().getExtension());
-
-      for (MarvelUrl marvelUrl : crr.getUrls()) {
-        if (marvelUrl.getType().equals("detail")) {
-          builder.detailsUrl(marvelUrl.getUrl());
-        }
-      }
-
-      for (ComicPrice comicPrice : crr.getComicPrices()) {
-        if (comicPrice.getType().equals("printPrice")) {
-          builder.price(comicPrice.getPrice());
-          Log.d("debug", "" + comicPrice.getPrice());
-        }
-      }
-      comicList.add(builder.build());
+            @Override
+            public void onFailure(Call<MarvelResponseModel<HeroResponseResult>> call, Throwable t) {
+                callback.onFailure();
+            }
+        });
     }
-    return comicList;
-  }
+
+    @Override
+    public void getComics(@NonNull final FetchDataCallback callback, int fromComicId) {
+        Call<MarvelResponseModel<ComicResponseResult>> call = mMarvelApi.listOfComicsResponse(ApiUtility.getRequestQueryParam(fromComicId));
+        call.enqueue(new Callback<MarvelResponseModel<ComicResponseResult>>() {
+            @Override
+            public void onResponse(Call<MarvelResponseModel<ComicResponseResult>> call, Response<MarvelResponseModel<ComicResponseResult>> response) {
+                callback.onSuccess(mapComicResponseResultsIntoComics(response.body().getData().getResults()));
+            }
+
+            @Override
+            public void onFailure(Call<MarvelResponseModel<ComicResponseResult>> call, Throwable t) {
+                callback.onFailure();
+            }
+        });
+    }
+
+    private List<Hero> mapHeroResponseResultsIntoHeroes(List<HeroResponseResult> heroResponseResultList) {
+        List<Hero> heroList = new ArrayList<>();
+        Hero.Builder builder;
+        for (HeroResponseResult hrr : heroResponseResultList) {
+            builder = new Hero.Builder(hrr.getId(), hrr.getName())
+                    .description(hrr.getDescription())
+                    .thumbnailPath(hrr.getThumbnail().getPath())
+                    .thumbnailExtention(hrr.getThumbnail().getExtension());
+
+            for (MarvelUrl marvelUrl : hrr.getUrls()) {
+                if (marvelUrl.getType().equals("detail")) {
+                    builder.detailsUrl(marvelUrl.getUrl());
+                }
+            }
+            heroList.add(builder.build());
+        }
+        return heroList;
+    }
+
+    private List<Comic> mapComicResponseResultsIntoComics(List<ComicResponseResult> comicResponseResultList) {
+        List<Comic> comicList = new ArrayList<>();
+
+        Comic.Builder builder;
+        for (ComicResponseResult crr : comicResponseResultList) {
+            builder = new Comic.Builder(crr.getId(), crr.getTitle())
+                    .description(crr.getDescription())
+                    .thumbnailPath(crr.getThumbnail().getPath())
+                    .thumbnailExtention(crr.getThumbnail().getExtension());
+
+            for (MarvelUrl marvelUrl : crr.getUrls()) {
+                if (marvelUrl.getType().equals("detail")) {
+                    builder.detailsUrl(marvelUrl.getUrl());
+                }
+            }
+
+            for (ComicPrice comicPrice : crr.getComicPrices()) {
+                if (comicPrice.getType().equals("printPrice")) {
+                    builder.price(comicPrice.getPrice());
+                    Log.d("debug", "" + comicPrice.getPrice());
+                }
+            }
+            comicList.add(builder.build());
+        }
+        return comicList;
+    }
 }
